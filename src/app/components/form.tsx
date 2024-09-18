9e47bc396b833f36f18cbce11efcffab77179bc9
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2024-09-19 01:51:44 +0900

    firebase-profile-dbsend

--- src/app/components/form.tsx
+++ src/app/components/form.tsx
@@ -2,9 +2,10 @@
 import React from 'react';
 import { collection, addDoc } from 'firebase/firestore';
 import { db } from '../../lib/firebase';
+
 export const ProfileForm = () => {
 
-  const DataSend =(event: React.FormEvent<HTMLFormElement>)=>{  
+  const DataSend = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();   
     const formData = new FormData(event.currentTarget);  
     
@@ -14,44 +15,38 @@ export const ProfileForm = () => {
     const birthDay = Number(formData.get("birthDay"));
     const gender = Number(formData.get("gender"));
 
-
-    try{        
-      const docRef = addDoc(collection(db,"users"),{    
-        nickname:nickname,
-        birthYear:birthYear,
-        birthMonth:birthMonth,
+    try {        
+      const docRef = await addDoc(collection(db, "users"), {
+        nickname,
+        birthYear,
+        birthMonth,
         birthDay,
         gender,
       });
-    } catch(e){    
-      console.error('Error adding document: ', e);  
+      console.log('Document written with ID: ', docRef.id);
+    } catch (e) {    
+      console.error('Error adding document: ', e);
     }
   }
 
-
-
   return (
     <form className="bg-yellow p-6" onSubmit={DataSend}>
-
       <div>
         <input className="border" aria-label="nickname" name='nickname' type="text" />
       </div>
-      
       <div>
-        <input className="border"  aria-label="nickname" name='birthYear' type="number" />
-        <input className="border"  aria-label="nickname" name='birthMonth' type="number" />
-        <input className="border" aria-label="nickname" name='birthDay' type="number" />  
+        <input className="border" aria-label="birthYear" name='birthYear' type="number" />
+        <input className="border" aria-label="birthMonth" name='birthMonth' type="number" />
+        <input className="border" aria-label="birthDay" name='birthDay' type="number" />  
       </div>
-
       <div>
-        <input className="border" aria-label="mail" value={0} name='gender' type='radio' />
-        <input className="border" aria-label="femail" value={1} name='gender' type='radio' />
+        <input className="border" aria-label="male" value={0} name='gender' type='radio' />
+        <input className="border" aria-label="female" value={1} name='gender' type='radio' />
         <input className="border" aria-label="other" value={2} name='gender' type='radio' />
       </div>
-
       <div>
         <input className="border" value={'登録'} type='submit' />
       </div>
     </form>
-  )
+  );
 }
