4fd6b8e0211025e82d3a92390cc605a7c98bb061
hiromuota166 <otahiromu166@gmail.com>
2024-09-18 14:33:27 +0900

    add: Googleログイン機能飲み実装

---
+++ src/app/debug/page.tsx
@@ -0,0 +1,27 @@
+"use client";
+import { useAuth } from "@/context/auth";
+import { login, logout } from "@/lib/auth";
+import { useState } from "react";
+
+export default function Home() {
+  const user = useAuth();
+  const [waiting, setWaiting] = useState<boolean>(false);
+
+  const signIn = () => {
+    setWaiting(true);
+
+    login()
+      .catch((error) => {
+        console.error(error?.code);
+      })
+      .finally(() => {
+        setWaiting(false);
+      });
+  };
+  return (
+    <div>
+      {user === null && !waiting && <button onClick={signIn}>ログイン</button>}
+      {user && <button onClick={logout}>ログアウト</button>}
+    </div>
+  );
+}
