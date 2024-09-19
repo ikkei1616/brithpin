64aca66cfc94e4767a95218ce1e511d2064ad780
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2024-09-20 02:27:30 +0900

    accoun表示

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,17 +1,68 @@
-import React from 'react'
-import Footer from '../components/Footer';
+"use client";
+import { doc, getDoc } from "firebase/firestore";
+import { db, auth } from "../../lib/firebase";
+import React from "react";
+import Footer from "../components/Footer";
+import { useEffect, useState } from "react";
+
+interface User {
+  nickname: string;
+  birthDay: number;
+  birthMonth: number;
+  birthYear: number;
+  gender: number;
+  photoURL: string;
+}
 
 const AccountPage = () => {
+
+  const [userData,setUserData] = useState<User | undefined>(undefined);
+
+  const getDocData = async() => {
+    if (auth.currentUser === null) {
+      console.log("あいあい居合")
+      return;
+    }
+    const docRef = doc(db, "users",auth.currentUser.uid);
+    const docSnap =  await getDoc(docRef);
+    console.log(docSnap)
+    if (docSnap.exists()){
+      const docSnapData = docSnap.data();
+      const formatUserData :User ={
+        nickname:docSnapData.nickname,
+        birthDay:docSnapData.birthDay,
+        birthMonth:docSnapData.birthMonth,
+        birthYear:docSnapData.birthYear,
+        gender:docSnapData.gender,
+        photoURL:docSnapData.photoURL,
+      };
+      setUserData(formatUserData);
+    }else{
+      console.log("No such document!");
+    }
+  };
+
+  useEffect(() => {
+    getDocData();
+  }, []);
+
+
   return (
-    <div className='h-screen flex items-center justify-center pt-6'>
+    <div className="h-screen flex items-center justify-center pt-6">
       <div>
         {/* <ProfileForm /> */}
+        <p>{userData?.nickname}</p>
+        <p>{userData?.birthYear}</p>
+        <p>{userData?.birthMonth}</p>
+        <p>{userData?.birthDay}</p>
+        <p>{userData?.gender}</p>
+        <p>{userData?.photoURL}</p>
       </div>
-      <div className='fixed bottom-0 w-full'>
+      <div className="fixed bottom-0 w-full">
         <Footer />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AccountPage
+export default AccountPage;
