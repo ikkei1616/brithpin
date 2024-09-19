9db908ebc9629bca0817ffe2e97866f7a8a0b20f
hiromuota166 <otahiromu166@gmail.com>
2024-09-19 16:43:42 +0900

    add:友達がいるユーザーのみ表示

--- src/app/api/notification/route.ts
+++ src/app/api/notification/route.ts
@@ -1,6 +1,24 @@
 import { NextRequest, NextResponse } from "next/server";
-import { initializeApp, cert, applicationDefault } from "firebase-admin/app";
+import { initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+
+// User型の定義
+interface User {
+  id: string;
+  nickname: string;
+  friends?: string[]; // フレンドのIDの配列
+  birthMonth?: number;
+  birthDay?: number;
+  friendsDetails?: FriendDetail[]; // フレンドの詳細情報の配列
+}
+
+// FriendDetail型の定義
+interface FriendDetail {
+  id: string;
+  nickname: string;
+  birthMonth?: number;
+  birthDay?: number;
+}
 
 // Firebase Admin SDKの初期化
 const serviceAccount = require("/firebaseSecretKey.json");
@@ -12,12 +30,38 @@ const db = getFirestore();
 
 export async function GET(req: NextRequest) {
   try {
-    // Firestoreからユーザー情報を取得
+    // Firestoreから全ユーザー情報を取得
     const usersSnapshot = await db.collection("users").get();
-    const usersData = usersSnapshot.docs.map((doc) => ({
+    let usersData: User[] = usersSnapshot.docs.map((doc) => ({
       id: doc.id,
-      ...doc.data(),
+      nickname: doc.data().nickname,
+      friends: doc.data().friends || [],
+      birthMonth: doc.data().birthMonth,
+      birthDay: doc.data().birthDay,
     }));
+
+    // 各ユーザーのフレンド情報を取得
+    for (const user of usersData) {
+      if (user.friends && user.friends.length > 0) {
+        // フレンドのIDからフレンド情報を取得
+        const friendsSnapshot = await db.collection("users")
+          .where('__name__', 'in', user.friends)
+          .get();
+
+        // フレンド情報の抽出
+        user.friendsDetails = friendsSnapshot.docs.map(friendDoc => ({
+          id: friendDoc.id,
+          nickname: friendDoc.data().nickname,
+          birthDay: friendDoc.data().birthDay,
+          birthMonth: friendDoc.data().birthMonth,
+        }));
+      } else {
+        user.friendsDetails = [];
+      }
+    }
+
+    // friendsDetailsが空でないユーザーのみをフィルタリング
+    usersData = usersData.filter(user => user.friendsDetails && user.friendsDetails.length > 0);
 
     // データをレスポンスとして返す
     return NextResponse.json({ users: usersData });
