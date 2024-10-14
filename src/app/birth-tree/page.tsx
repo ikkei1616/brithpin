0852a5cd4d8a14979721ede2cc0850e0d96367f2
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2024-10-14 11:54:14 +0900

    フォーマットの修正

--- src/app/birth-tree/page.tsx
+++ src/app/birth-tree/page.tsx
@@ -1,12 +1,12 @@
-'use client';
-import * as React from 'react';
+"use client";
+import * as React from "react";
 import AuthContext, { fetchFriendsID, fetchFriends } from "@/context/auth";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Button from '@mui/material/Button';
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Button from "@mui/material/Button";
 
 export type FriendSchema = {
   id: string;
@@ -15,23 +15,23 @@ export type FriendSchema = {
   nickname: string;
   name: string;
   photoURL: string;
-}
+};
 
 export default function BirthTree() {
   const authValue = useContext(AuthContext);
-  const router = useRouter()
+  const router = useRouter();
   const [friends, setFriends] = useState<FriendSchema[]>([]);
-  const [openModalId,setOpenModalId] =useState<string | null>(null)
+  const [openModalId, setOpenModalId] = useState<string | null>(null);
 
   const handleOpen = (id: string) => {
     setOpenModalId(id);
   };
-  const handleClose = ()=> {
+  const handleClose = () => {
     setOpenModalId(null);
   };
   const onClickRoute = () => {
-    router.push('/account')
-  }
+    router.push("/account");
+  };
 
   const maxLeftValue = 100;
   const maxTopValue = 100;
@@ -45,16 +45,16 @@ export default function BirthTree() {
     { left: 75, top: 57 },
     { left: 40, top: 67 },
     { left: 10, top: 73 },
-  ]
+  ];
 
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
     width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    bgcolor: "background.paper",
+    border: "2px solid #000",
     boxShadow: 24,
     pt: 2,
     px: 4,
@@ -63,7 +63,6 @@ export default function BirthTree() {
 
   useEffect(() => {
     (async () => {
-
       const friendIDList: string[] = await fetchFriendsID();
 
       fetchFriends(friendIDList);
@@ -72,46 +71,67 @@ export default function BirthTree() {
 
       // フレンドリストのデータを保存
       setFriends(friendList);
-
-    })()
+    })();
   }, [authValue]);
 
-  const sortFriendsByBirthday = (friendList: FriendSchema[]): FriendSchema[] => {
+  const sortFriendsByBirthday = (
+    friendList: FriendSchema[]
+  ): FriendSchema[] => {
     const today = new Date();
 
     return friendList.sort((a, b) => {
-      const dateA = new Date(today.getFullYear(), a.birthMonth - 1, a.birthDay + 1);
-      const dateB = new Date(today.getFullYear(), b.birthMonth - 1, b.birthDay + 1);
+      const dateA = new Date(
+        today.getFullYear(),
+        a.birthMonth - 1,
+        a.birthDay + 1
+      );
+      const dateB = new Date(
+        today.getFullYear(),
+        b.birthMonth - 1,
+        b.birthDay + 1
+      );
 
       // 今年の誕生日がまだ来ているかどうか判断
-      const nextBirthdayA = dateA >= today ? dateA : new Date(today.getFullYear() + 1, a.birthMonth - 1, a.birthDay);
-      const nextBirthdayB = dateB > today ? dateB : new Date(today.getFullYear() + 1, b.birthMonth - 1, b.birthDay);
+      const nextBirthdayA =
+        dateA >= today
+          ? dateA
+          : new Date(today.getFullYear() + 1, a.birthMonth - 1, a.birthDay);
+      const nextBirthdayB =
+        dateB > today
+          ? dateB
+          : new Date(today.getFullYear() + 1, b.birthMonth - 1, b.birthDay);
 
       return nextBirthdayA.getTime() - nextBirthdayB.getTime();
     });
   };
 
   const sortedFriends = sortFriendsByBirthday(friends);
-  const sortedFriendsWithBirthDayFlag = sortedFriends.map(friend => {
+  const sortedFriendsWithBirthDayFlag = sortedFriends.map((friend) => {
     const today = new Date();
-    const isBirthDayToday = (today.getMonth() + 1 === friend.birthMonth) && (today.getDate() === friend.birthDay);
+    const isBirthDayToday =
+      today.getMonth() + 1 === friend.birthMonth &&
+      today.getDate() === friend.birthDay;
     return {
       ...friend,
-      isBirthDayToday
-    }
-  })
+      isBirthDayToday,
+    };
+  });
   // 実行
   return (
     <div className="flex w-screen h-screen  justify-center ">
       <div className="wapper">
         <div className="background">
-          <Button onClick={onClickRoute}
+          <Button
+            onClick={onClickRoute}
             style={{
               position: "absolute",
               left: 20,
               minWidth: 70,
-            }}>
-            <Image src="/top-button.svg" alt="Button Image"
+            }}
+          >
+            <Image
+              src="/top-button.svg"
+              alt="Button Image"
               width={100}
               height={50}
             />
@@ -124,14 +144,14 @@ export default function BirthTree() {
               <>
                 <Button
                   key={friend.id}
-                  onClick={()=> handleOpen(friend.id)}      
+                  onClick={() => handleOpen(friend.id)}
                   sx={{
-                    height:"100px",
+                    height: "100px",
                     position: "absolute",
                     left: `${(locate[index].left / maxLeftValue) * 100}%`,
                     top: `${(locate[index].top / maxTopValue) * 100}%`,
                   }}
-                > 
+                >
                   <div>
                     <Image
                       src="/fukidashi.png"
@@ -141,7 +161,7 @@ export default function BirthTree() {
                         position: "absolute",
                         top: -50,
                         minWidth: 70,
-                        pointerEvents:"none"
+                        pointerEvents: "none",
                       }}
                       height={100}
                       width={100}
@@ -187,34 +207,33 @@ export default function BirthTree() {
                     />
                   </div>
                 </Button>
-                  <Modal
-                    open={openModalId === friend.id}
-                    onClose={handleClose}
-                    aria-labelledby="child-modal-title"
-                    aria-describedby="child-modal-description"
-                  >  
-                    <Box sx={{ ...style, width: 200 }}>
-                      <h2 id="child-modal-title">{friend.name}さんへ</h2>
-                      <form action="#">
-                        <textarea 
-                          id={`message"-${friend.id}`}
-                          name="message" 
-                          rows={5}
-                          title='aaa'
-                          cols={15}
-                          style={{
-                            border: "2px solid #000"
-                          }}
-                        >
-                        </textarea>
-                      </form>
-                      <Button onClick={handleClose}>閉じる</Button>
-                      <Button >提出</Button>
-                    </Box>
-                  </Modal>
+                <Modal
+                  open={openModalId === friend.id}
+                  onClose={handleClose}
+                  aria-labelledby="child-modal-title"
+                  aria-describedby="child-modal-description"
+                >
+                  <Box sx={{ ...style, width: 200 }}>
+                    <h2 id="child-modal-title">{friend.name}さんへ</h2>
+                    <form action="#">
+                      <textarea
+                        id={`message"-${friend.id}`}
+                        name="message"
+                        rows={5}
+                        title="aaa"
+                        cols={15}
+                        style={{
+                          border: "2px solid #000",
+                        }}
+                      ></textarea>
+                    </form>
+                    <Button onClick={handleClose}>閉じる</Button>
+                    <Button>提出</Button>
+                  </Box>
+                </Modal>
               </>
             );
-          })} 
+          })}
         </div>
       </div>
     </div>
