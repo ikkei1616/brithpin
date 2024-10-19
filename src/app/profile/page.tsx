6ecb51da431330df624030e1be8f9b73a341285c
hiromuota166 <otahiromu166@gmail.com>
2024-10-19 21:24:55 +0900

    add: userのログイン状態のルーティング

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,8 +1,20 @@
 'use client';
 import React from 'react'
 import { ProfileForm } from '../../components/ProfileForm';
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from "@/context/auth";
 
 const ProfilePage = () => {
+  const router = useRouter();
+  const user = useAuth();
+  useEffect(() => {
+    if (user) {
+      router.push('/profile');
+    } else{
+      router.push('/');
+    }
+  }, [user, router]);
   return (
     <div className='h-screen flex items-center justify-center pt-6'>
       <div>
