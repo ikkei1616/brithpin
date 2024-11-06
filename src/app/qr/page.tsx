9aebebc8a9f73199458470a6d23e7a8a2e5ff632
hiromuota166 <otahiromu166@gmail.com>
2024-11-06 16:13:06 +0900

    add: qr画面の色変更

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -6,6 +6,7 @@ import { QRCodeSVG } from 'qrcode.react';
 import { db, auth } from '../../lib/firebase';
 import { onAuthStateChanged } from "firebase/auth";
 import { doc, getDoc } from 'firebase/firestore';
+import { useColorContext } from '@/context/ColorContext';
 
 const QrPage = () => {
   const [userId, setUserId] = useState<string>("");
@@ -15,24 +16,21 @@ const QrPage = () => {
   const [birthMonth, setBirthMonth] = useState<number | null>(null);
   const [birthDay, setBirthDay] = useState<number | null>(null);
   const router = useRouter();
+  const { colors } = useColorContext();
 
-  // Firebaseの認証状態を監視し、uidを取得
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         setUid(user.uid);
-        console.log("認証ユーザーのUID:", user.uid);  // uidのデバッグログ
-        await fetchUserData(user.uid);  // ユーザーデータを取得
+        await fetchUserData(user.uid);
       } else {
         setUid(null);
-        console.log("ログインしているユーザーがいません");  // ログアウト状態のデバッグログ
       }
     });
 
     return () => unsubscribe();
   }, []);
 
-  // Firestoreからユーザーデータを取得する関数
   const fetchUserData = async (uid: string) => {
     try {
       const docRef = doc(db, "users", uid);
@@ -40,18 +38,15 @@ const QrPage = () => {
 
       if (docSnap.exists()) {
         const data = docSnap.data();
-        console.log("取得したユーザーデータ:", data);  // デバッグ用に全データをログ出力
         setNickname(data.nickname);
         setBirthYear(data.birthYear);
         setBirthMonth(data.birthMonth);
         setBirthDay(data.birthDay);
-        console.log("Nickname:", data.nickname);  // nicknameのデバッグログ
-        console.log("BirthYear:", data.birthYear, "BirthMonth:", data.birthMonth, "BirthDay:", data.birthDay);  // 生年月日のデバッグログ
       } else {
-        console.log("ユーザーデータが存在しません");  // データが存在しない場合のデバッグログ
+        console.log("ユーザーデータが存在しません");
       }
     } catch (error) {
-      console.log("データ取得エラー:", error);  // エラーログ
+      console.log("データ取得エラー:", error);
     }
   };
 
@@ -72,8 +67,8 @@ const QrPage = () => {
     <div className='h-screen flex relative flex-col items-center'>
       <div className='h-screen w-11/12 flex justify-center'>
         <div className="flex flex-col items-center justify-center h-[85%] mt-28">
-          <div className='max-w-md rounded-3xl border-mainpink border-2 shadow-lg px-10 py-3'>
-            <div className='text-center text-2xl text-textbrawn font-aboreto border-b border-mainpink border-dashed py-4'>
+          <div style={{ borderColor: colors.bg }} className='max-w-md rounded-3xl border-2 shadow-lg px-10 py-3'>
+            <div style={{ borderColor: colors.bg }} className='text-center text-2xl text-textbrawn font-aboreto border-b border-dashed py-4'>
               <span>PROFILE</span>
             </div>
             <div className="mt-3">
@@ -105,13 +100,15 @@ const QrPage = () => {
             <label className="block text-textbrawnlight text-lg mt-1 font-serif">ID検索</label>
             <div className=''>
               <input
+                style={{ borderColor: colors.bg }}
                 type="text"
-                className="p-2 pr-16 border border-mainpink rounded-lg w-full text-sm shadow-md"
+                className="p-2 pr-16 border rounded-lg w-full text-sm shadow-md"
                 onChange={handleInputChange}
                 value={userId}
               />
               <button
-                className="absolute right-2 top-3/4 transform -translate-y-1/2 bg-pin text-color rounded-full w-6 h-6 flex justify-center bg-mainpink"
+                style={{ background: colors.bg }}
+                className="absolute right-2 top-3/4 transform -translate-y-1/2 bg-pin text-color rounded-full w-6 h-6 flex justify-center"
                 onClick={getSearch}
               >
                 ✓
