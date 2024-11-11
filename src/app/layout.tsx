0f30c3be719ee16892cbee642a7583ba7c74d50a
hiromuota166 <otahiromu166@gmail.com>
2024-11-11 10:52:47 +0900

    delete: build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,17 +5,6 @@ import { AuthProvider } from "@/context/auth";
 import { Layout } from "@/components/Layout";
 import { ColorProvider } from '@/context/ColorContext';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata: Metadata = {
   title: "Birth PIN",
   description: "とくべつな日を もっとたのしく",
