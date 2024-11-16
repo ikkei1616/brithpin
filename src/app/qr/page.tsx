505c6afbd1e5d40045778a8a6da5235fe530eab6
hiromuota166 <otahiromu166@gmail.com>
2024-11-16 22:06:29 +0900

    add: qr画面スッキリさせた

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -67,8 +67,8 @@ const QrPage = () => {
     <div className='h-screen flex relative flex-col items-center'>
       <div className='h-screen w-11/12 flex justify-center'>
         <div className="flex flex-col items-center justify-center h-[85%] mt-28">
-          <div style={{ borderColor: colors.bg }} className='max-w-md rounded-3xl border-2 shadow-lg px-10 py-3'>
-            <div style={{ borderColor: colors.bg }} className='text-center text-2xl text-textbrawn font-aboreto border-b border-dashed py-4'>
+          <div style={{ borderColor: colors.bg }} className='max-w-md rounded-3xl border-2 shadow-lg px-16 py-3'>
+            <div style={{ borderColor: colors.bg }} className='text-center text-2xl text-textbrawn font-aboreto border-b border-dashed py-2'>
               <span>PROFILE</span>
             </div>
             <div className="mt-3">
@@ -86,38 +86,11 @@ const QrPage = () => {
             <div className="flex justify-center items-center my-4">
               <div className="flex flex-col items-center justify-center px-3 pb-3rounded-md">
                 <QRCodeSVG value={`https://brithpin.vercel.app/add-friend/${uid || "default"}`} size={120} />
-                {uid && (
-                  <div className="text-xs font-serif text-textbrawnlight mt-2">
-                    ID : {uid}
-                  </div>
-                )}
               </div>
-            </div>
-          </div>
-
-          {/* ID検索の横幅をCardContainerに合わせる */}
-          <div className="relative w-full">
-            <label className="block text-textbrawnlight text-lg mt-1 font-serif">ID検索</label>
-            <div className=''>
-              <input
-                style={{ borderColor: colors.bg }}
-                type="text"
-                className="p-2 pr-16 border rounded-lg w-full text-sm shadow-md bg-inherit"
-                onChange={handleInputChange}
-                value={userId}
-              />
-              <button
-                style={{ background: colors.bg }}
-                className="absolute right-2 top-3/4 transform -translate-y-1/2 bg-pin text-color rounded-full w-6 h-6 flex justify-center"
-                onClick={getSearch}
-              >
-                ✓
-              </button>
             </div>
           </div>
         </div>
       </div>
-
       {/* フッター部分を固定して浮かせる */}
       <div className="w-full absolute bottom-0">
         <Footer />
